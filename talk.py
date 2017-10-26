a1236a51e20780a9172f3dfe68c0fa9d4a69d20c
tomlum <checkeredlines@gmail.com>
2017-10-25 23:35:09 -0400

    init

---
+++ talk.py
@@ -0,0 +1,79 @@
+# Adapted from Lucashu1
+from keras.models import load_model
+import numpy as np
+import random
+import sys
+import codecs
+
+corpus = "speare.txt"
+fileName = "speare-V1-HL128-BS128-A1.h5"
+genLength = 500
+diversity = .5
+
+lookBack = 20
+skip = 3
+
+###############################################################
+#  Organize Corpus
+###############################################################
+
+text = codecs.open(corpus).read().lower()
+chars = sorted(list(set(text)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+sentences = []
+next_chars = []
+for i in range(0, len(text) - lookBack, skip):
+    sentences.append(text[i: i + lookBack])
+    next_chars.append(text[i + lookBack])
+
+print("Vectorization...")
+X = np.zeros((len(sentences), lookBack, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+print("Building Model...")
+model = load_model(fileName)
+
+###############################################################
+#  Generating
+###############################################################
+
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype("float64")
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
+print("Diversity is " + str(diversity))
+
+start_index = random.randint(0, len(text) - lookBack - 1)
+generated = ""
+sentence = text[start_index: start_index + lookBack]
+
+print("----- Generating with seed: " + sentence)
+sys.stdout.write(generated)
+print("------------------------------------------------")
+
+for i in range(genLength):
+    x = np.zeros((1, lookBack, len(chars)))
+    for t, char in enumerate(sentence):
+        x[0, t, char_indices[char]] = 1.
+
+    preds = model.predict(x, verbose=0)[0]
+    next_index = sample(preds, diversity)
+    next_char = indices_char[next_index]
+
+    generated += next_char
+    sentence = sentence[1:] + next_char
+
+    sys.stdout.write(next_char)
+    sys.stdout.flush()
---
+++ train.py
@@ -0,0 +1,100 @@
+# Adapted from Lucashu1
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+from keras.models import load_model
+import numpy as np
+import codecs
+
+corpus = "speare-short.txt"
+version = 1
+# How many epochs its run through
+age = 0
+hiddenLayers = 128
+learningRate = 0.001
+batchSize = 128
+# How many characters back to consider when generating the next
+lookBack = 20
+# How many characters to skip after a read in the document
+skip = 3
+
+epochs = 50
+
+###############################################################
+#  Organize Corpus
+###############################################################
+
+text = codecs.open(corpus).read().lower()
+print("Corpus Length:", len(text))
+
+chars = sorted(list(set(text)))
+print("Total Chars:", len(chars))
+
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+sentences = []
+next_chars = []
+for i in range(0, len(text) - lookBack, skip):
+    sentences.append(text[i: i + lookBack])
+    next_chars.append(text[i + lookBack])
+print("Total Sequences:", len(sentences))
+
+print("Vectorization...")
+X = np.zeros((len(sentences), lookBack, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+###############################################################
+#  Build LSTM
+###############################################################
+
+
+if age > 0:
+    print("Loading Model, Age:" + str(age) + "...")
+    # Load previous save
+    model = load_model(
+        corpus +
+        "-V" + str(version) +
+        "-HL" + str(hiddenLayers) +
+        "-LR" + str(learningRate) +
+        "-BS" + str(batchSize) +
+        "-LB" + str(lookBack) +
+        "-SK" + str(skip) +
+        "-A" + str(age) +
+        ".h5")
+else:
+    print("Building Model...")
+    # Or construct new LSTM
+    model = Sequential()
+    model.add(LSTM(hiddenLayers, input_shape=(lookBack, len(chars))))
+    model.add(Dense(len(chars)))
+    model.add(Activation("softmax"))
+    optimizer = RMSprop(lr=learningRate)
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+
+###############################################################
+#  Training
+###############################################################
+
+for iteration in range(1, epochs):
+    age = age + 1
+    print("Iteration ", iteration)
+    model.fit(X, y,
+              batch_size=batchSize,
+              epochs=1,
+              verbose=2)
+    model.save(
+        corpus +
+        "-V" + str(version) +
+        "-HL" + str(hiddenLayers) +
+        "-LR" + str(learningRate) +
+        "-BS" + str(batchSize) +
+        "-LB" + str(lookBack) +
+        "-SK" + str(skip) +
+        "-A" + str(age) +
+        ".h5")
